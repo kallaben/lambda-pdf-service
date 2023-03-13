601d48a4979fee5fcfec09799bee9d38cd59bcb9
kallaben <didironja@hotmail.com>
2023-03-13 13:40:46 +0100

    Use chrome-aws-lambda

--- src/app.js
+++ src/app.js
@@ -1,53 +1,15 @@
-const puppeteer = require("puppeteer");
+const chromium = require("chrome-aws-lambda");
 const fs = require("fs");
 
 async function lambdaHandler(event, context) {
   console.log("Started");
 
   // Create a new browser instance using Puppeteer
-  const browser = await puppeteer.launch({
-    executablePath: "google-chrome-stable",
-    headless: true,
+  const browser = await chromium.puppeteer.launch({
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
     dumpio: true, // pass chrome logs to output, helps a lot if launch fails
-    args: [
-      "--autoplay-policy=user-gesture-required",
-      "--disable-background-networking",
-      "--disable-background-timer-throttling",
-      "--disable-backgrounding-occluded-windows",
-      "--disable-breakpad",
-      "--disable-client-side-phishing-detection",
-      "--disable-component-update",
-      "--disable-default-apps",
-      "--disable-dev-shm-usage",
-      "--disable-domain-reliability",
-      "--disable-extensions",
-      "--disable-features=AudioServiceOutOfProcess",
-      "--disable-hang-monitor",
-      "--disable-ipc-flooding-protection",
-      "--disable-notifications",
-      "--disable-offer-store-unmasked-wallet-cards",
-      "--disable-popup-blocking",
-      "--disable-print-preview",
-      "--disable-prompt-on-repost",
-      "--disable-renderer-backgrounding",
-      "--disable-setuid-sandbox",
-      "--disable-speech-api",
-      "--disable-sync",
-      "--disk-cache-size=33554432",
-      "--hide-scrollbars",
-      "--ignore-gpu-blacklist",
-      "--metrics-recording-only",
-      "--mute-audio",
-      "--no-default-browser-check",
-      "--no-first-run",
-      "--no-pings",
-      "--no-sandbox",
-      "--no-zygote",
-      "--password-store=basic",
-      "--use-gl=swiftshader",
-      "--use-mock-keychain",
-      "--single-process",
-    ],
+    args: chromium.args,
   });
 
   console.log("Launched browser");
